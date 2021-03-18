e9f4ce589e8699b739ee77e597227950330dd093
Racer <reaganjoseph26@yahoo.com>
2021-03-17 20:36:22 -0400

    Contact component can conditionally render

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Gallery from './compenents/gallery';
 import ContactForm from './compenents/Contact';
 
 function App() {
+  // false means to prevent the contact form from showing when a user initially navigates to the homepage.
+  const [contactSelected, setContactSelected] = useState(false);
+
   const [categories] = useState([
     {
       name: 'commercial',
@@ -20,14 +23,22 @@ function App() {
   return (
     <div>
       <Nav
-        categories={categories}
-        setCurrentCategory={setCurrentCategory}
-        currentCategory={currentCategory}
+       categories={categories}
+       setCurrentCategory={setCurrentCategory}
+       currentCategory={currentCategory}
+       contactSelected={contactSelected}
+       setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <ContactForm></ContactForm>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
+      {!contactSelected ? (
+          // shorthand for <React.Fragment></React.Fragment>.
+            <>
+              <Gallery currentCategory={currentCategory}></Gallery>
+              <About></About>
+            </>
+          ) : (
+              <ContactForm></ContactForm>
+            )}
       </main>
     </div>
   );
